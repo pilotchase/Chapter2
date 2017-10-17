8b5ebdef9381133a6da71256a56340ae5122ea9d
Chase Morgan <pilotchase@users.noreply.github.com>
2017-10-17 11:56:23 -0700

    Final Commit

---
+++ RightTriange.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+// *******************************************************************
+//   RightTriangle.java
+//
+//   Compute the length of the hypotenuse of a right triangle
+//   given the lengths of the sides
+// *******************************************************************
+
+public class RightTriangle
+{
+    public static void main (String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        double side1, side2;  // lengths of the sides of a right triangle
+        double hypotenuse;        // length of the hypotenuse
+
+        // Get the lengths of the sides as input
+        System.out.print ("Please enter the length of the side of a right triangle");
+        side1 = keyboard.nextDouble();
+
+
+
+        // Compute the length of the hypotenuse
+
+
+
+        // Print the result
+        System.out.println ("Length of the hypotenuse: " + hypotenuse);
+    }
+}
