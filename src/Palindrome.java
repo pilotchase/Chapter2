f9cffc16f8a421b5427d74a1112ce631e8a6f444
Chase Morgan <pilotchase@users.noreply.github.com>
2017-10-17 11:55:47 -0700

    Initial commit

---
+++ src/Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    private string word;
+    private boolean isPalindrome;
+
+    public Palindrome(String wd)
+    {
+        this.word = wd;
+    }
+
+    public boolean testWord()
+    {
+        // logic to test Palindrome
+
+        // Srt instance variable
+        return isPalindrome;
+    }
+}
